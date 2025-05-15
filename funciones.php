ddbf649aa6788122bf312cb33dc8115bd242894c
GonzaloA48 <gonzalo-asero@hotmail.com>
2025-05-15 14:09:07 -0500

    first commit

---
+++ funciones.php
@@ -0,0 +1,38 @@
+<?php
+    //Funcion sin parametros
+
+    function saludo (){
+        echo "<H2>"."Buenas tardes, la fecha de hoy es: "."</H2>";
+        echo date ('Y-M-D');
+    }
+
+    echo saludo();
+
+    //FUNCION CON ENVIO PARAMETROS 
+        function saludar ($nombre,$apellido){
+            echo "<h2>" ."Buenas tardes " .$nombre." ".$apellido."</h2>";
+
+        }
+        echo saludar("Gonzalo" , "Asero");
+        echo saludar("Luis" , "Yepez");
+        
+        function multiplicar($a,$b,$c){
+            echo "<h2>" ."El resultdo es:" .$a*$b*$c.  "</h2>";
+            
+        }
+        echo multiplicar (5,2,4);
+        echo multiplicar (5,7,5);
+        //Funcion area de un circulo
+
+        function areaCirculo($radio){
+            echo "el area del circulo es:" .pi()*pow($radio,2);
+
+        }
+        $r = 5;
+        echo areaCirculo($r);
+
+
+    echo "<h1>"."FUNCIONES EN PHP"."</h1><br>";
+    
+
+?>
---
+++ inicio.php
@@ -0,0 +1,35 @@
+<?php
+    //phpinfo();
+    echo "hola mundo <br>";
+    echo "aprendiendo php <br>";
+    echo "inicio de php con errores <br>";
+    echo "las variables se nombran <br>";
+    echo "Imprime valores de forma directa <br>";
+    echo "Se pueden declarar arrays <br>";
+    echo "<h1>"."APRENDIENDO PHP". "</h1><br>";
+    echo "<h1>". "OPERADORES DE CONTROL"."</h1>";
+    echo "<h1>" . "NO LLEVAN EL CONTROL". "</h1>";
+    echo "<h1>"."ESTRUCTURAS DE CONTROL"."</h1>";
+    echo "constantes php <br>";
+    echo "caracteristica de php";
+    echo "<h1>". "INTRODUCCION DE PHP"."</h1>";
+    //VARIABLES DE PHP
+    $nombre = "gonzalo";
+    $edad = "16";
+    $raza ="trigueño";
+    $nacionalidad = "ecuatoriano";
+    echo "Mi nombre es ".$nombre. " y mi edad es " .$edad. "años<br>";
+    echo " Mi nombre es " .$nombre. " y mi edad es " .$edad. " años  <br> ";
+    echo " soy ".$raza. " Mi nombre es ".$nombre. " y mi edad es ".$edad. "años <br>";
+    echo " soy ".$nacionalidad. " de raza ".$raza. " mi nombre es " .$nombre. " y mi edad es " .$edad. " años <br>";
+
+    if($edad >=18)
+        echo "mayor de edad";
+    else
+        echo "menor de edad";
+
+
+
+
+
+?>
